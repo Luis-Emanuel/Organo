228972af953c7ecd2437c97fdc7a8eeb8fecc6c6
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2023-06-30 17:56:29 -0300

    .

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+const Banner = () => {
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt=""/>
+        </header>
+    )
+}
+
+export default Banner
